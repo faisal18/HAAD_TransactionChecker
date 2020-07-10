93c1bda84a8c3b75ed4119a3abaa3a4701cadaa9
M.Faisal Ansari <faisalansari.m@hotmail.com>
2020-07-10 22:06:08 +0400

    fa: test

--- HAAD_TransactionChecker/HaadSearchthis.cs
+++ HAAD_TransactionChecker/HaadSearchthis.cs
@@ -24,6 +24,7 @@ namespace HAAD_TransactionChecker
             int transactionType = 8;// RA
 
 
+
             try
             {
                 username = System.Configuration.ConfigurationManager.AppSettings.Get("username");
